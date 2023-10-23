1200a3b771b371d8b0a12a5d933e7ca339f05bda
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2023-10-23 17:51:03 +0200

    :tada: Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col justify-center items-center w-page min-h-screen">
+    <p class="text-2xl font-bold">Welcome to the Green School!</p>
+    <div class="flex flex-wrap mt-12 w-[41rem] gap-4">
+      <div class="flex justify-center items-center w-[20rem] h-40 bg-green-100 rounded-2xl">Sector 1</div>
+      <div class="flex justify-center items-center w-[20rem] h-40 bg-green-100 rounded-2xl">Sector 2</div>
+      <div class="flex justify-center items-center w-[20rem] h-40 bg-green-100 rounded-2xl">Sector 3</div>
+      <div class="flex justify-center items-center w-[20rem] h-40 bg-green-100 rounded-2xl">Sector 4</div>
+    </div>
+
+    <div class="flex items-center justify-center w-full mt-12">
+      <div class="bg-gray-300 w-[80%] h-1 rounded-full"></div>
+    </div>
+  </div>
+</template>
