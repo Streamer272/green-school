86ebe3b0747546a35ea5d8ed0ad32f3cb77800dc
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2023-11-01 20:36:06 +0100

    :lipstick: Fixes the sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,27 +1,35 @@
 <template>
   <button
     @click="() => (open = !open)"
-    :data-open="open"
-    class="absolute top-4 left-4 w-12 h-12 z-10 data-[open=true]:ml-60 transition-all"
+    class="flex items-center justify-center gap-x-6 absolute top-10 left-10 z-20 font-source font-semibold text-white text-xl"
   >
-    <img src="/icons/ham-menu.svg" alt="Menu" />
+    <img
+      :data-open="open"
+      src="/icons/menu.png"
+      alt="Menu"
+      class="data-[open=true]:rotate-90 transition"
+    />
+
+    Projects
   </button>
 
   <div
     :data-open="open"
-    class="flex flex-col justify-start items-start absolute top-0 left-0 w-60 h-full py-3 px-2 custom-bg custom-sidebar -translate-x-full data-[open=true]:translate-x-0 z-20 transition"
+    class="flex flex-col justify-start items-center absolute top-0 left-0 w-[28.125rem] h-full pt-[6.5rem] pb-2 bg-black custom-sidebar -translate-x-full data-[open=true]:translate-x-0 z-10 transition gap-y-4"
   >
-    <a>Hello World Project</a>
-    <a>NatArt Project</a>
-    <a>Portfolio Project</a>
+    <div
+      v-for="project in projects"
+      class="flex items-center justify-start gap-x-4 py-4 px-6 bg-dark rounded-[1.25rem] w-[23.125rem]"
+    >
+      <p class="font-source font-bold text-xl text-white">{{ project.name }}</p>
+      <p class="font-source font-semibold text-lg text-disc">
+        {{ project.start }}
+      </p>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.custom-bg {
-  background-color: rgb(148, 201, 59, 0.75);
-}
-
 .custom-sidebar {
   a {
     font-size: 1.15rem;
@@ -31,4 +39,21 @@
 
 <script lang="ts" setup>
 const open = useState("sidebar", () => false);
+const projects = [
+  {
+    name: "Project 1",
+    start: "23.08.2006",
+    description: "This is the project of my life.",
+  },
+  {
+    name: "Project 2",
+    start: "04.08.2022",
+    description: "This is the project of exploring.",
+  },
+  {
+    name: "Project 3",
+    start: "01.09.2025",
+    description: "This is the project of improving.",
+  },
+];
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     colors: {
       white: "#e0e0e0",
       light: "#efefef",
+      unim: "#b3b3b3",
+      disc: "#969696",
       gray: "#555555",
+      dark: "#222222",
+      despair: "#1b1b1b",
+      black: "#161616",
     },
     fontFamily: {
       source: ["'Source Sans 3'", "'sans-serif'"],
