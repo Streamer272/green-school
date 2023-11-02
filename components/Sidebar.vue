476405c9d40e16e3b3c93276b5909eaaeccf98ce
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2023-11-02 08:20:26 +0100

    :sparkles: Adds expanding projects

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -18,12 +18,36 @@
     class="flex flex-col justify-start items-center absolute top-0 left-0 w-[28.125rem] h-full pt-[6.5rem] pb-2 bg-black custom-sidebar -translate-x-full data-[open=true]:translate-x-0 z-10 transition gap-y-4"
   >
     <div
-      v-for="project in projects"
-      class="flex items-center justify-start gap-x-4 py-4 px-6 bg-dark rounded-[1.25rem] w-[23.125rem]"
+      v-for="(project, index) in projects"
+      class="bg-despair rounded-[1.25rem] w-[23.125rem]"
     >
-      <p class="font-source font-bold text-xl text-white">{{ project.name }}</p>
-      <p class="font-source font-semibold text-lg text-disc">
-        {{ project.start }}
+      <div
+        class="flex items-center justify-start gap-x-4 py-4 px-6 bg-dark rounded-[1.25rem] w-full"
+      >
+        <p class="font-source font-bold text-xl text-white">
+          {{ project.name }}
+        </p>
+        <p class="font-source font-semibold text-lg text-disc">
+          {{ project.start }}
+        </p>
+        <div class="flex-grow" />
+
+        <button @click="changeCurrentProject(index)">
+          <img
+            :data-open="currentProject === index"
+            src="/icons/arrow-right.png"
+            alt="Arrow right"
+            class="data-[open=true]:rotate-90 transition"
+          />
+        </button>
+      </div>
+
+      <!-- TODO: animate me -->
+      <p
+        :data-open="currentProject === index"
+        class="font-source font-semibold m-4 text-unim data-[open=false]:hidden"
+      >
+        {{ project.description }}
       </p>
     </div>
   </div>
@@ -39,6 +63,7 @@
 
 <script lang="ts" setup>
 const open = useState("sidebar", () => false);
+const currentProject = ref(-1);
 const projects = [
   {
     name: "Project 1",
@@ -56,4 +81,9 @@ const projects = [
     description: "This is the project of improving.",
   },
 ];
+
+function changeCurrentProject(index: number) {
+  if (currentProject.value === index) currentProject.value = -1;
+  else currentProject.value = index;
+}
 </script>
