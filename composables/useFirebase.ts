40e28bde294b966363d09e98a66f940d9771d877
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2023-11-05 13:45:39 +0100

    :tada: Inits firebase

---
+++ composables/useFirebase.ts
@@ -0,0 +1,23 @@
+import { getApp, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBwokOILzago40HKeM7KOCuTjzdgAQpz2U",
+  authDomain: "green-school-f3e14.firebaseapp.com",
+  projectId: "green-school-f3e14",
+  storageBucket: "green-school-f3e14.appspot.com",
+  messagingSenderId: "999756024706",
+  appId: "1:999756024706:web:54b12526acb338b9631d9b",
+};
+
+export function initFirebase() {
+  initializeApp(FIREBASE_CONFIG);
+}
+
+export function useFirebase() {
+  return getApp();
+}
+
+export function useFirestore() {
+  return getFirestore();
+}
