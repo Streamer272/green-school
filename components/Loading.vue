87fcbba085517089ff862b19894f27a0a52b9d9b
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2023-11-07 17:50:02 +0100

    :construction: Adds auth

--- components/Loading.vue
+++ components/Loading.vue
@@ -1,6 +1,12 @@
 <template>
-  <!-- TODO: add an actual loading animation -->
-  <div v-if="property === undefined">Loading</div>
+  <!-- TODO: fix the goofy ahh loading animation -->
+  <div v-if="property === undefined" class="flex items-center justify-center">
+    <img
+      src="/images/leaf-animation.gif"
+      alt="Rotating leaf"
+      class="w-64 h-48"
+    />
+  </div>
   <slot v-else />
 </template>
 
