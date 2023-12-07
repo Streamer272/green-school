fce3776e6e873c83aaf8d3ea64fecf6535158a23
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2023-12-07 12:29:24 +0100

    :construction: Adds script to new timeline

--- pages/admin/timeline/new.vue
+++ pages/admin/timeline/new.vue
@@ -1,5 +1,40 @@
-<script setup lang="ts"></script>
-
 <template></template>
 
-<style scoped lang="scss"></style>
+<script lang="ts" setup>
+import { addDoc, collection } from "@firebase/firestore";
+import { useFirestore } from "~/composables/useFirebase";
+
+const name = ref("");
+const description = ref("");
+const icon = ref("");
+const start = ref(0);
+const end = ref(0);
+const files = ref([]);
+const fileName = ref("");
+const fileType = ref("");
+const fileLink = ref("");
+const loading = ref(false);
+
+function submit(event: Event) {
+  event.preventDefault();
+  loading.value = true;
+
+  addDoc(collection(useFirestore(), "themes"), {
+    name: name.value,
+    description: description.value,
+    icon: icon.value,
+    start: start.value,
+    end: end.value,
+    files: files.value,
+  })
+    .then(() => {
+      navigateTo("/admin/timeline");
+    })
+    .catch((err) => {
+      loading.value = false;
+      alert(err);
+    });
+}
+
+useAuthGuard(true);
+</script>
